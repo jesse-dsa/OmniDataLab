70cc9b5511f87e7dc62cbdaef319cb238f06007a
jesse-dsa <jesse.dsa.research@gmail.com>
2025-04-28 11:07:45 -0300

    fix: corrige formatação e conexão com o PostgreSQL no teste

--- tests/integration/test_postgresql_connection.py
+++ tests/integration/test_postgresql_connection.py
@@ -1,6 +1,7 @@
 import psycopg2
 import pytest
 
+
 def test_postgresql_connection():
     try:
         # Configurações de conexão com o banco de dados
@@ -9,7 +10,7 @@ def test_postgresql_connection():
             user="your_user",  # Substitua com seu nome de usuário
             password="Jotta@1440",  # Substitua com sua senha
             host="127.0.0.1",  # Ou o IP do servidor
-            port="5432"  # O padrão do PostgreSQL
+            port="5432",  # O padrão do PostgreSQL
         )
         # Se a conexão for bem-sucedida, fecha a conexão
         connection.close()
